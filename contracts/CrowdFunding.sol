1686e2eb686df321cd6e59a80b9327b5ab18d57b
Hasan0734 <Jahidhasan2856@gmail.com>
2024-10-12 21:54:29 -0700

    added send eth donate

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract CrowdFunding {
+
+         mapping (address => uint) public contributors;
+         address public  manager;
+         uint public minimumContribution;
+         uint public deadline;
+         uint public target;
+         uint public raisedAmount;
+         uint  public noOfContributors;
+
+         constructor(uint _target, uint _deadline){
+            target = _target;
+            deadline = block.timestamp+_deadline;
+            minimumContribution = 100 wei;
+            manager =  msg.sender;
+         }
+
+         function sendEth() public   payable  {
+            require(block.timestamp < deadline, "Deadline has passed");
+            require(msg.value >= minimumContribution, "Minimum Contribution is not met");
+
+            if(contributors[msg.sender] == 0){
+                noOfContributors++;
+            }
+            contributors[msg.sender]+=msg.value;
+            raisedAmount+=msg.value;
+         }
+
+
+}               
